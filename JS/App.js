465677f33d86e387e66bb35314e6243be68b7641
ALEX <skanderchebil1@gmail.com>
2021-08-25 23:54:19 +0100

    dropdown styling done (bootstrap technical base)

--- JS/App.js
+++ JS/App.js
@@ -1,13 +1,22 @@
-searchBar = document.querySelector('#search');
-searchWall = document.querySelector('.searchWall');
-main = document.querySelector('main');
+const searchBar = document.querySelector('#search');
+const searchWall = document.querySelector('.searchWall');
+const searchIcon = document.querySelector('#searchIcon');
+const backspaceIcon = document.querySelector('#backspaceIcon');
+const main = document.querySelector('main');
+
 
 
 
 searchBar.addEventListener('click', () => {
-     searchWall.classList.toggle("active")
+     searchWall.classList.toggle("active");
+     searchIcon.classList.toggle("active");
+     backspaceIcon.classList.toggle("active");
+
 });
 
 main.addEventListener('click', () => {
-     searchWall.classList.remove("active")
+     searchWall.classList.remove("active");
+     searchIcon.classList.remove('active');
+     backspaceIcon.classList.remove("active");
+
 });
