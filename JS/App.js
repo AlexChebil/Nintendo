3b66d1b8a84570a10fdf9f46d0c6d9d8aeb36448
ALEX <skanderchebil1@gmail.com>
2021-09-03 00:57:09 +0100

    nearly fully finished the nav (little bug created with the desktop version, will solve it later)

--- JS/App.js
+++ JS/App.js
@@ -2,6 +2,10 @@ const searchBar = document.querySelector('#search');
 const searchWall = document.querySelector('.searchWall');
 const searchIcon = document.querySelector('#searchIcon');
 const backspaceIcon = document.querySelector('#backspaceIcon');
+const searchButtonMediaQ = document.querySelector('.searchButtonMediaQ');
+const burgerMenu = document.querySelector('.burgerMenu');
+const nav = document.querySelector('nav');
+
 const main = document.querySelector('main');
 
 
@@ -11,12 +15,30 @@ searchBar.addEventListener('click', () => {
      searchWall.classList.toggle("active");
      searchIcon.classList.toggle("active");
      backspaceIcon.classList.toggle("active");
-
 });
 
 main.addEventListener('click', () => {
      searchWall.classList.remove("active");
      searchIcon.classList.remove('active');
      backspaceIcon.classList.remove("active");
-
 });
+
+
+searchButtonMediaQ.addEventListener('click',()=>{
+     searchWall.classList.toggle("active");
+})
+
+searchButtonMediaQ.addEventListener('mouseover', ()=>{
+     searchIcon.classList.add("active");
+})
+
+searchButtonMediaQ.addEventListener('mouseout', ()=>{
+     searchIcon.classList.remove("active");
+})
+
+burgerMenu.addEventListener('click',()=>{
+     nav.classList.toggle("active");
+     searchWall.classList.remove("active");
+
+
+})
