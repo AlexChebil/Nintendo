23fdcb5db85c81acb2bb6f8eec69bd24f46c0fb0
ALEX <skanderchebil1@gmail.com>
2021-10-02 00:42:37 +0100

    huge leap on the lower games caroussel still some minor upgrades and changes and it's done

--- JS/Main-JS.js
+++ JS/Main-JS.js
@@ -1,6 +1,11 @@
-const carousselButton1 = document.getElementById('button1')
-const carousselButton2 = document.getElementById('button2')
+const carousselButton1 = document.getElementById('button1');
+const carousselButton2 = document.getElementById('button2');
 const caroussel = document.querySelector('.cont');
+
+const lowcarousselButton1 = document.getElementById('lowCaroussel-button1');
+const lowcarousselButton2 = document.getElementById('lowCaroussel-button2');
+const lowcaroussel = document.querySelector('.lowCaroussel-flexCont');
+
 
 
 
@@ -9,4 +14,12 @@ carousselButton2.addEventListener('click',()=>{
 });
 carousselButton1.addEventListener('click',()=>{
      caroussel.classList.remove("active")
+});
+
+lowcarousselButton1.addEventListener('click',()=>{
+     lowcaroussel.classList.add("active")
+});
+
+lowcarousselButton2.addEventListener('click',()=>{
+     lowcaroussel.classList.remove("active")
 });
